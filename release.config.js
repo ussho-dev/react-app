55dcd45bddad7d0a66dc13576579a703a9b18d77
ussho-dev <ussho.dev@gmail.com>
2021-12-09 11:11:32 +0700

    feat: init commitlint and commitizen

--- release.config.js
+++ release.config.js
@@ -1,4 +1,18 @@
 module.exports = {
   branches: "master",
   repositoryUrl: "https://github.com/ussho-dev/react-app.git",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "Coverage" },
+        ],
+      },
+    ],
+  ],
 };
